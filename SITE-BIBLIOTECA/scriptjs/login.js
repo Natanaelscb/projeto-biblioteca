2a0ec99be7cbdeef4bdfad26ffd7a79a0597657f
Natanaelscb <js573028@gmail.com>
2026-03-17 19:18:27 -0300

    teste

--- SITE-BIBLIOTECA/scriptjs/login.js
+++ SITE-BIBLIOTECA/scriptjs/login.js
@@ -8,10 +8,12 @@ document.getElementById("login").addEventListener("submit", function(event){
     let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
 
-    let encontrado = users.find(u=>u.usuario === usuario && u.senha === senha); /*find() significa procurar algo dentro da lista. Aqui u representa cada usuário da lista. u.usuario === usuario. Aqui ele compara:*/
+    /*find() significa procurar algo dentro da lista. Aqui u representa cada usuário da lista. u.usuario === usuario. Aqui ele compara:*/
+    let encontrado = users.find(u=>u.usuario === usuario && u.senha === senha);
 
+    /*window representa a janela do navegador E location representa o endereço da página (URL).*/ 
     if(encontrado){
-        window.location.href = "../inicio/index.html" /*window representa a janela do navegador E location representa o endereço da página (URL).*/ 
+        window.location.href = "../inicio/index.html" 
     } else{
         alert('USER OU SENHA ERRADOS TENTE NOVAMENTE...')
 
